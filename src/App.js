8f03d2acf5dd9c686e0c46024154d7ed3fcd4ba5
Alisson Henrique <alisson-henri@live.com>
2019-10-16 17:27:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
-
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import history from './services/history';
+import './config/ReactotronConfig';
 function App() {
   return (
-    <div className="App">
-      teste
-    </div>
-  );
+    <Router history={history}>
+      <Routes />
+    </Router>
+  )
 }
 
 export default App;
